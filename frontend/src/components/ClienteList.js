c93a6a687fc68bb4c2617c39700e87636e03e673
Sergio Villafan <villafan815@gmail.com>
2025-03-12 21:11:05 -0400

    Tarea Clase

--- frontend/src/components/ClienteList.js
+++ frontend/src/components/ClienteList.js
@@ -11,6 +11,36 @@ const ClienteList = () => {
       .catch((error) => console.error('Error al obtener clientes', error));
   }, []);
 
+  const handleView = (id) => {
+    console.log(id);
+  };
+
+  const handleEdit = (id) => {
+    updateUser(id, {nombre: 'Updated'});
+  };
+
+  const handleDelete = (id) => {
+    deleteUser(id);
+  };
+
+  const updateUser = (id, data) => {
+    axios.put(`http://localhost:3000/clientes/${id}`, data)
+      .then(() => updateTable())
+      .catch((error) => console.error('Error al editar cliente', error));
+  };
+
+  const deleteUser = (id) => {
+    axios.delete(`http://localhost:3000/clientes/${id}`)
+      .then(() => updateTable())
+      .catch((error) => console.error('Error al eliminar cliente', error));
+  };
+
+  const updateTable = () => {
+    axios.get('http://localhost:3000/clientes')
+      .then((response) => setClientes(response.data))
+      .catch((error) => console.error('Error al obtener clientes', error));
+  }
+
   return (
     <div className="table-container">
       <h2>Lista de Clientes</h2>
@@ -20,6 +50,9 @@ const ClienteList = () => {
             <th>ID</th>
             <th>Nombre</th>
             <th>Correo</th>
+            <th>Ver</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
           </tr>
         </thead>
         <tbody>
@@ -28,6 +61,9 @@ const ClienteList = () => {
               <td>{cliente.id}</td>
               <td>{cliente.nombre}</td>
               <td>{cliente.correo}</td>
+              <td><button onClick={() => handleView(cliente.id)}>Ver</button></td>
+              <td><button onClick={() => handleEdit(cliente)}>Editar</button></td>
+              <td><button onClick={() => handleDelete(cliente.id)}>Eliminar</button></td>
             </tr>
           ))}
         </tbody>
